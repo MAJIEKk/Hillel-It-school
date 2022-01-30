70d55e3f1245d1addf8da432219c4a5c015df578
MAJIEKk <94832132+MAJIEKk@users.noreply.github.com>
2022-01-30 14:58:37 +0200

    Delete ht1 — копия directory

--- ht1 — копия/main.js
+++
@@ -1,6 +0,0 @@
-var yourName = prompt("Enter your name:");
-alert("Hello " + yourName);
-
-var user1 = document.getElementById('userName');
-console.log(user1.getO);
-user1.innerHTML = "Let's work " + yourName;
