a23bbffb43c8adce1edfd6a0cb33240013b46c44
MAJIEKk <94832132+MAJIEKk@users.noreply.github.com>
2022-01-30 14:57:46 +0200

    Add files via upload

---
+++ ht1 — копия/main.js
@@ -0,0 +1,6 @@
+var yourName = prompt("Enter your name:");
+alert("Hello " + yourName);
+
+var user1 = document.getElementById('userName');
+console.log(user1.getO);
+user1.innerHTML = "Let's work " + yourName;
