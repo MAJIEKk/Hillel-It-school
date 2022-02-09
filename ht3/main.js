b52f43b05c80a5fe2a18393687ed55034f0430f4
MAJIEKk <94832132+MAJIEKk@users.noreply.github.com>
2022-02-09 09:22:43 +0200

    Add files via upload

--- ht3/main.js
+++ ht3/main.js
@@ -7,22 +7,22 @@ function phoneF() {
     phoneNumber = prompt("Enter your phone number:", '+380671234567');
     if (phoneNumber.length < 13 || phoneNumber.length > 13) {
         alert("Please enter '+' and 12 numbers");
-        phoneF();
+        return phoneF();
     }
     if (nationality && phoneNumber.slice(0, 4) != "+380") {
         alert("Your phone suppose to start whith '+380...'!");
-        phoneF();
+        return phoneF();
     }
     for (let i in phoneNumber) {
         if (phoneNumber[0] != "+") {
             alert("Please start whith '+'!")
-            phoneF();
+            return phoneF();
         }
         if (i != 0 && (phoneNumber[i].charCodeAt() < 48 || phoneNumber[i].charCodeAt() > 57)) {
             alert("Please enter NUMBERS, not simbols or letter");
-            phoneF();
+            return phoneF();
         }
     }
     alert("Yoohoo! You did it!");
     document.getElementById('phoneBTN').innerHTML = `Сlick to leave a <a style="color: red; font-weight: bold;">NEW</a> number</br>current: <a style="font-weight: bold;">${phoneNumber}</a>`;
-}
+    }
