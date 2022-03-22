7d1778e908091a9af504bbc53651b1b998fd0d22
MAJIEKk <94832132+MAJIEKk@users.noreply.github.com>
2022-03-22 23:28:41 +0200

    Загрузил дз9 и классную

---
+++ classwork/main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+let block = document.querySelector("h1");
+console.dir(block);
+block.onclick = function(){
+    // alert('hello');
+    // if(this.className==="aaa"){
+    //     this.className="";
+    // }
+    // else this.className="aaa";
+this.trasform
+
+}
+style
+var block2 = {
+
+}
